e78e2eb313d06a632493172c1f3ca6293e54b6e1
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-11-16 15:12:37 -0500

    commit 3

--- src/com/skarlatov/Main.java
+++ src/com/skarlatov/Main.java
@@ -5,7 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+    Image i = new ProxyImage("somefile.jpg");
 
+    //image loade from disk
+        i.display();
+        System.out.println("");
+        i.display();
 
 
 
