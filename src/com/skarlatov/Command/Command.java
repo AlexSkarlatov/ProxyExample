0c630b200ed3fb345ad456e72cca3f17d5c39c52
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-11-25 16:05:34 -0500

    Challenge completed  extend and practice for memeorization of associations . chaining concept for seaparation of loosely coupling and responsibilities

--- src/com/skarlatov/Command/Command.java
+++ src/com/skarlatov/Command/Command.java
@@ -7,3 +7,56 @@ public interface Command {
     // possible to add an undor or redo commands
 
 }
+
+
+class OpenFileCommand implements Command {
+
+    //reference to a receiver object
+    private FileSystemReceiver fileSystem;
+
+    // store previouse state for undo operation
+    // somestate someState = null;
+    public OpenFileCommand(FileSystemReceiver fs){
+        this.fileSystem = fs;
+    }
+
+    @Override
+    public void execute() {
+        this.fileSystem.openFile();
+    }
+
+//    public void undo(){
+//        // restore previous state
+//        //mymbemer varaible  = previous state
+//    }
+
+}
+
+
+class CloseFileCommand implements Command {
+    private FileSystemReceiver fileSystem;
+
+    public CloseFileCommand(FileSystemReceiver fs){
+        this.fileSystem = fs;
+    }
+
+    @Override
+    public void execute() {
+        this.fileSystem.closeFile();
+    }
+}
+
+
+class WriteFileCommand implements Command {
+    FileSystemReceiver fileSystemReceiver ;
+
+    public WriteFileCommand(FileSystemReceiver fileSystemReceiver){
+        this.fileSystemReceiver = fileSystemReceiver;
+
+    }
+
+    @Override
+    public void execute() {
+        this.fileSystemReceiver.writeFile();
+    }
+}
